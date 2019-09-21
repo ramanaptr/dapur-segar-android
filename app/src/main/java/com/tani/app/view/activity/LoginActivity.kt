3dffb420644a2f06d7b3409e9c596b0c6a3e6779
Ramana Putra <ramana.putra@rebelworks.co>
2019-09-21 15:26:17 +0700

    first commit

---
+++ app/src/main/java/com/tani/app/view/activity/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.tani.app.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tani.app.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+
+    }
+}
