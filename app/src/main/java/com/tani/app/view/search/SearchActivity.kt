86aefaa2b03b9c6edf4db00986a435df1075fe2d
Ramana Putra <ramana.putra@rebelworks.co>
2019-10-17 10:11:55 +0700

    Slicing of search layout

--- app/src/main/java/com/tani/app/view/search/SearchActivity.kt
+++ app/src/main/java/com/tani/app/view/search/SearchActivity.kt
@@ -2,10 +2,19 @@ package com.tani.app.view.search
 
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexboxLayoutManager
 import com.tani.app.R
 import com.tani.app.base.activity.BaseActivity
+import com.tani.app.view.search.adapter.TagAdapter
+import com.tani.app.view.search.model.TagItem
+import kotlinx.android.synthetic.main.search_activity.*
+import org.jetbrains.anko.toast
 
 class SearchActivity : BaseActivity() {
+
+    private var tagAdapter: TagAdapter? = null
 
     override fun statusBarColor(): Int = R.color.white
 
@@ -14,11 +23,36 @@ class SearchActivity : BaseActivity() {
     override fun setLayout(): Int = R.layout.search_activity
 
     override fun onCreate(savedInstanceState: Bundle?, actionBar: ActionBar?) {
+        setupAdapter()
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return super.onSupportNavigateUp()
     }
 
     override fun onClick() {
 
+    }
+
+    private fun setupAdapter() {
+        val data = arrayListOf<TagItem>().apply {
+            add(TagItem("Fruit"))
+            add(TagItem("Vegetable"))
+            add(TagItem("Durian"))
+            add(TagItem("Fresh Fruit"))
+            add(TagItem("Good Quality"))
+            add(TagItem("Apple"))
+            add(TagItem("For Me"))
+        }
+        tagAdapter = TagAdapter { toast(it.tagName) }.apply { setData(data) }
+        rvTag.apply {
+            val glm = GridLayoutManager(baseContext, 2)
+            val flm = FlexboxLayoutManager(baseContext, FlexDirection.ROW)
+            layoutManager = flm
+            adapter = tagAdapter
+            isNestedScrollingEnabled = false
+        }
     }
 
 }
---
+++ app/src/main/java/com/tani/app/view/search/model/TagItem.kt
@@ -0,0 +1,5 @@
+package com.tani.app.view.search.model
+
+data class TagItem(
+    val tagName : String
+)
