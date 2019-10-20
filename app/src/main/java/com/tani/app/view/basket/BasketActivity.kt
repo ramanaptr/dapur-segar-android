74a2cadb5ac205264a9c36d129ec9f3c5c5e3c94
Ramana Putra <ramana.putra@rebelworks.co>
2019-10-20 19:16:12 +0700

    Adding basket alert, and basket checkout layout

--- app/src/main/java/com/tani/app/view/basket/BasketActivity.kt
+++ app/src/main/java/com/tani/app/view/basket/BasketActivity.kt
@@ -1,7 +1,6 @@
-package com.tani.app.view.payment
+package com.tani.app.view.basket
 
 import android.os.Bundle
-import android.view.Menu
 import androidx.appcompat.app.ActionBar
 import com.tani.app.R
 import com.tani.app.base.activity.BaseActivity
@@ -10,16 +9,21 @@ import com.tani.app.base.activity.BaseActivity
  * Created by Ramana on 20-Oct-19.
  */
  
-class PaymentDetailActivity : BaseActivity(){
+class BasketActivity  : BaseActivity() {
 
     override fun statusBarColor(): Int = R.color.white
 
-    override fun setTitle(): String = getString(R.string.payment_detail)
+    override fun setTitle(): String = getString(R.string.basket_title)
 
-    override fun setLayout(): Int = R.layout.payment_detail
+    override fun setLayout(): Int = R.layout.basket_activity
 
     override fun onCreate(savedInstanceState: Bundle?, actionBar: ActionBar?) {
         initComponent()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return super.onSupportNavigateUp()
     }
 
     override fun onClick() {
