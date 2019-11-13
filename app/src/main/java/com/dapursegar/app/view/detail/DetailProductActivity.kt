e1f5b286d5c7a62b39ab1cb4c7212f3f93f621da
Ramana Putra <ramanaputra@gmail.com>
2019-11-13 09:46:17 +0700

    Add packet on detail product to choose

--- app/src/main/java/com/dapursegar/app/view/detail/DetailProductActivity.kt
+++ app/src/main/java/com/dapursegar/app/view/detail/DetailProductActivity.kt
@@ -6,15 +6,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.dapursegar.app.R
 import com.dapursegar.app.base.activity.BaseActivity
 import com.dapursegar.app.base.adapter.BannersAdapter
+import com.dapursegar.app.base.dialog.WeightAdapter
 import com.dapursegar.app.base.extension.settingToolbar
 import com.dapursegar.app.model.home.ProductItem
 import com.dapursegar.app.view.detail.dialog.BSProductQuantity
 import com.dapursegar.app.view.main.home.adapter.ProductAdapter
 import kotlinx.android.synthetic.main.activity_detail_product.*
-import kotlinx.android.synthetic.main.activity_detail_product.tvWeightPrice
 import kotlinx.android.synthetic.main.default_add_quantity.*
 import kotlinx.android.synthetic.main.detail_product_toolbar.*
-import kotlinx.android.synthetic.main.dialog_product_quantity.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
@@ -26,6 +25,8 @@ class DetailProductActivity : BaseActivity<DetailProductViewModel>() {
 
     private var quantity = 1
     private var bannersAdapter: BannersAdapter? = null
+    private var productAdapter: ProductAdapter? = null
+    private var weightAdapter: WeightAdapter? = null
     private val bannersDummy = mutableListOf<Int>().apply {
         add(R.drawable.durian)
         add(R.drawable.fresh_fruit)
@@ -41,6 +42,7 @@ class DetailProductActivity : BaseActivity<DetailProductViewModel>() {
         settingToolbar(tvTitle, "Dummy Apel Malang", toolbar)
         initComponent()
         setupAdapter()
+        loadData()
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -48,12 +50,22 @@ class DetailProductActivity : BaseActivity<DetailProductViewModel>() {
         return super.onSupportNavigateUp()
     }
 
-    private fun loadData(): MutableList<ProductItem> {
+    private fun loadData() {
         val products = mutableListOf<ProductItem>()
         for (category in 1..5) {
             products.add(ProductItem(R.drawable.durian, "Durian Bali"))
         }
-        return products
+        productAdapter?.setData(products)
+
+        val prices = mutableListOf<String>().apply {
+            add("500 Gm - IDR 40.000")
+            add("1 Kg - IDR 130.100")
+            add("2 Kg - IDR 230.100")
+            add("3 Kg - IDR 340.000")
+        }
+        weightAdapter?.setData(prices)
+
+        bannersAdapter?.setBanners(bannersDummy)
     }
 
     override fun onClick() {
@@ -74,21 +86,31 @@ class DetailProductActivity : BaseActivity<DetailProductViewModel>() {
     }
 
     private fun setupAdapter() {
-        rvSimilarProduct.apply {
-            adapter = ProductAdapter { onClickProduct(this, it) }.apply { setData(loadData()) }
-            isNestedScrollingEnabled = false
-            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        }
         vpSlide.apply {
-            bannersAdapter = BannersAdapter(BannersAdapter.PRODUCT).apply {
-                setBanners(bannersDummy)
-            }
+            bannersAdapter = BannersAdapter(BannersAdapter.PRODUCT)
             adapter = bannersAdapter
             piv.apply {
                 radius = 4
                 padding = 8
             }
         }
+        rvSimilarProduct.apply {
+            productAdapter =
+                ProductAdapter { onClickProduct(this, it) }
+            adapter = productAdapter
+            isNestedScrollingEnabled = false
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        }
+        rvProductOrder.apply {
+            weightAdapter = WeightAdapter { viewState(true) }
+            adapter = weightAdapter
+            isNestedScrollingEnabled = false
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    private fun viewState(state: Boolean) {
+
     }
 
     private fun onClickProduct(data: ProductItem, state: String) {
